cbcea492a0153adb48fa6dec74ea61877e5ef6ca
Ricky Codes <rickydoescode@gmail.com>
2020-06-18 18:07:19 +0700

    fixed extra spacing from texts

--- index.js
+++ index.js
@@ -20,16 +20,18 @@ client.on('message', message => {
 	        .setFooter('Made with ❤️️');
     if (message.content.indexOf(prefix) > -1) {
         const text = message.content.replace(prefix, "");
+        let toSend = [];
         switch (text) {
             case 'test':
-                embed.setDescription(`Oh hello there!`)
+                embed.setDescription('Oh hello there!')
                     .setColor("#005bbe");
                 break;
             case 'server': 
-                embed.setDescription(`
-                Server name: ${message.guild.name}
-                Total members: ${message.guild.memberCount}
-                `)
+                toSend = [
+                    `Server name: ${message.guild.name}`,
+                    `Total members: ${message.guild.memberCount}`,
+                ]
+                embed.setDescription(toSend.join("\n"))
                 .setColor("#19eb3b");
                 break;
             case 'random':
@@ -38,19 +40,21 @@ client.on('message', message => {
                 embed.setImage(`https://picsum.photos/id/${id}/500`)
                 break;
             case 'help':
-                embed.setDescription(`
-                    test - returns a message to check if bot's working
-                    server - provide server detail (name and user count)
-                    random - gets random picture
-                `)
+                toSend = [
+                    'test - returns a message to check if bot\'s working',
+                    'server - provide server detail (name and user count)',
+                    'random - gets random picture'
+                ]
+                embed.setDescription(toSend.join('\n'))
                     .setColor("#f1d400");
                 break;
             default:
-                embed.setDescription(`
-                    Oops!
-                    We didn't find that command!
-                    Try ${prefix}help for more info!
-                `)
+                toSend = [
+                    'Oops!',
+                    'We didn\'t find that command!',
+                    'Try ${prefix}help for more info!'
+                ]
+                embed.setDescription(toSend.join('\n'))
                     .setColor("#df0000");
                 break;
         }
