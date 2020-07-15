604fbcb182d09e8ea09c7fc6c9153e5e9ec8c6db
root <root@bump-bot.asia-southeast2-a.c.rickycodes-rc.internal>
2020-07-15 06:53:38 +0000

    update all file and scripts

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV == 'dev') {
-    require('dotenv').config();
-}
+require('dotenv').config();
 
 const { Client,MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
